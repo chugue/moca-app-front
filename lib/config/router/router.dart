60ed0bbffb08438681b9322e91e40318085fe886
chugue <chugue85@gmail.com>
2025-04-13 22:49:46 +0900

    [PAGE] activity screen

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,8 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:moca/ui/activities/activities_screen.dart';
 import 'package:moca/ui/search/search_screen.dart';
 
-final routerProvider = GoRouter(
+final _router = GoRouter(
+  initialLocation: '/activities',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SearchScreen()),
+    GoRoute(
+      path: '/activities',
+      builder: (context, state) => const ActivitiesScreen(),
+    ),
   ],
 );
+
+final routerProvider = Provider<GoRouter>((ref) => _router);
---
+++ lib/ui/activities/activities_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:moca/ui/activities/widgets/activities_screen/activity_item.dart';
+import 'package:moca/ui/core/constants/color.dart';
+import 'package:moca/ui/core/constants/line.dart';
+import 'package:moca/ui/core/constants/style.dart';
+
+class ActivitiesScreen extends HookConsumerWidget {
+  const ActivitiesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('내 활동', style: AppTextStyle.sectionHeadline),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              ActivityItem(
+                title: '최근 본 카페',
+                icon: const Icon(Icons.history, color: AppColor.pointYellow),
+                onTap: () {},
+              ),
+              AppLines.thinDivider,
+              ActivityItem(
+                title: '좋아요',
+                icon: const Icon(Icons.favorite, color: AppColor.pointYellow),
+                onTap: () {},
+              ),
+              AppLines.thinDivider,
+              ActivityItem(
+                title: '스크랩',
+                icon: const Icon(Icons.bookmark, color: AppColor.pointYellow),
+                onTap: () {},
+              ),
+              AppLines.thinDivider,
+              ActivityItem(
+                title: '내 리뷰',
+                icon: const Icon(Icons.comment, color: AppColor.pointYellow),
+                onTap: () {},
+              ),
+              AppLines.thinDivider,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
