0cc0e5405b50ffded5da8e4276af7cc9bacff73d
chugue <chugue85@gmail.com>
2025-04-13 23:42:58 +0900

    [CONFLICT] solved with jyj/search

--- lib/config/router/router.dart
+++ lib/config/router/router.dart
@@ -1,23 +1,22 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:moca/ui/activities/activities_screen.dart';
 import 'package:moca/ui/search/search_screen.dart';
 import '../../ui/search/screens/search_detail_screen.dart';
 
-final routerProvider = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const SearchScreen(),
-    ),
-    GoRoute(
-      path: '/search-detail',
-      builder: (context, state) => const SearchDetailScreen(),
-    ),
-    GoRoute(
-      path: '/activities',
-      builder: (context, state) => const ActivitiesScreen(),
-    ),
-  ],
+final routerProvider = Provider<GoRouter>(
+  (ref) => GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const SearchScreen()),
+      GoRoute(
+        path: '/search-detail',
+        builder: (context, state) => const SearchDetailScreen(),
+      ),
+      GoRoute(
+        path: '/activities',
+        builder: (context, state) => const ActivitiesScreen(),
+      ),
+    ],
+  ),
 );
